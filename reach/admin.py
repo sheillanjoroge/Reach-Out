f2d28251a62df85c8d932c7e0a88b9d8aeb14184
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-02-03 08:30:36 +0300

    add auth and more admin functions

--- reach/admin.py
+++ reach/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import User, Business, Hood, Announcement, Profile, Meeting, Essential
  
-admin.site.register(User)
-admin.site.register(Hood)
-admin.site.register(Business)
-admin.site.register(Announcement)
+# admin.site.register(User)
+# admin.site.register(Hood)
+# admin.site.register(Business)
+# admin.site.register(Announcement)
 admin.site.register(Profile)
 admin.site.register(Meeting)
 admin.site.register(Essential)
